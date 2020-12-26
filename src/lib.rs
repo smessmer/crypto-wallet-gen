a435ed55bab1750ea056cc55b54141734e3168e8
Sebastian Messmer <mail@smessmer.de>
2020-12-26 22:44:56 +0100

    Setup circle CI

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,5 @@ mod seed_to_monero_wallet;
 pub use derive_hd_wallet::{derive_hd_wallet, Bip44DerivationPath, CoinType};
 pub use mnemonic_to_seed::{generate_mnemonic, mnemonic_to_seed};
 pub use seed::Seed;
-pub use seed_to_bitcoin_wallet::{seed_to_bitcoin_wallet, BitcoinWallet};pub use seed_to_monero_wallet::{seed_to_monero_wallet, MoneroWallet};
+pub use seed_to_bitcoin_wallet::{seed_to_bitcoin_wallet, BitcoinWallet};
+pub use seed_to_monero_wallet::{seed_to_monero_wallet, MoneroWallet};
